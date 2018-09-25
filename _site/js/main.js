cd5e1b3f694261958eea7a71c8b54873f14689fc
telagraphic <telagraphic@gmail.com>
2018-09-25 15:53:40 -0400

    sub-page structure and nav

--- _site/js/main.js
+++ _site/js/main.js
@@ -90,3 +90,19 @@ $('document').ready(function() {
 
 
 });
+
+
+var links = document.querySelectorAll('.main-nav a');
+
+links.forEach(function(link) {
+  link.addEventListener('click', function(event) {
+    removeActive(links);
+    this.classList.add('active');
+  })
+});
+
+function removeActive(links) {
+  links.forEach(function(link) {
+    link.classList.remove('active');
+  });
+}

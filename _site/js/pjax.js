138b9376e7585e02fbfe58e3848f1407648a0108
telagraphic <telagraphic@gmail.com>
2018-09-27 13:45:32 -0400

    setup for pjax

---
+++ _site/js/pjax.js
@@ -0,0 +1,10 @@
+//  get main-menu and intercept clicks
+
+
+var main_menu = document.querySelector(".main-menu");
+
+main_menu.addEventListener("click", function(event) {
+  if (event.target && event.target.matches("a.chapter-link")) {
+    
+  }
+});

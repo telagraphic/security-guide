d8d19fb4d20a49e86ddc0d6f813b52781ce680ce
telagraphic <telagraphic@gmail.com>
2018-10-02 13:21:02 -0400

    got sub-content code working, will need to refactor

--- _site/js/pjax.js
+++ _site/js/pjax.js
@@ -1,4 +1,4 @@
-// 
+//
 // console.log("location.pathname:" + location.pathname);
 // console.log("location.hash:" + location.hash);
 // console.log("location.host:" + location.host);
@@ -11,6 +11,8 @@
 //  get main-menu and intercept clicks
 var main_menu = document.querySelector(".main-menu");
 var page_container = document.querySelector(".page-container");
+var chapter_contents = document.querySelector(".chapter-contents");
+
 
 // container to intercept
 // link class to target
@@ -18,60 +20,73 @@ var page_container = document.querySelector(".page-container");
 //
 
 function setupChapterLinks() {
+  //main_menu
   main_menu.addEventListener("click", function(event) {
     if (event.target && event.target.matches("a.chapter-link")) {
       event.preventDefault();
+      var linkURL = event.target.getAttribute("href");
+      if (preventDoubleClick(linkURL)) {
+        fetchPage(linkURL, false);
+      };
     }
-
-    var linkURL = event.target.getAttribute("href");
-
-    // check if same page
-    if (preventDoubleClick(linkURL)) {
-      // hide current page
-      fetchPage(linkURL, false);
-    };
   });
 }
 
 var chapter_links, chapter_contents;
 
 window.addEventListener("click", function(event) {
-  if (event.target && event.target.nodeName === "A") {
+  if (event.target && event.target.matches("a.sub-link")) {
+    event.preventDefault();
     console.log("link clicked");
-    chapter_links = document.querySelector(".chapter-links");
-    chapter_contents = document.querySelector(".chapter-contents");
-  };
 
+    var subURL = event.target.getAttribute("href");
+    if (preventDoubleClick(subURL)) {
+      fetchSection(subURL, false);
+    }
+  };
 });
+
+
+
+window.addEventListener("click", function(event) {
+  if (event.target && event.target.matches("a.sub-page-link")) {
+    event.preventDefault();
+    var subURL = event.target.getAttribute("href");
+    if (preventDoubleClick(subURL)) {
+      fetchSection(subURL, false);
+    }
+  };
+});
+
 //
 // if (document.readyState !== 'loading') {
 //   console.log("content loaded");
 // }
 // console.log(chapter_links);
 // console.log(chapter_contents);
+//
+// var chapter_links = document.querySelector(".chapter-links");
+// var chapter_contents = document.querySelector(".chapter-contents");
 
-var chapter_links = document.querySelector(".chapter-links");
-var chapter_contents = document.querySelector(".chapter-contents");
-
-function setupSectionLinks() {
-
-  chapter_links.addEventListener("click", function(event) {
-    if (event.target && event.target.matches("a.sub-link")) {
-      event.preventDefault();
-    }
-
-    var linkURL = event.target.getAttribute("href");
-    console.log(linkURL);
-
-    // check if same page
-    if (preventDoubleClick(linkURL)) {
-      // hide current page
-      fetchSection(linkURL, false);
-    };
-  });
-}
-
-setupSectionLinks();
+// function setupSectionLinks() {
+//
+//   chapter_links.addEventListener("click", function(event) {
+//     if (event.target && event.target.matches("a.sub-link")) {
+//       event.preventDefault();
+//     }
+//
+//     var linkURL = event.target.getAttribute("href");
+//     console.log(linkURL);
+//
+//     // check if same page
+//     if (preventDoubleClick(linkURL)) {
+//       // hide current page
+//       fetchSection(linkURL, false);
+//     };
+//   });
+// }
+//
+// setupSectionLinks();
 
 function preventDoubleClick(url) {
   var newURL = url.split('/').pop();
@@ -113,8 +128,8 @@ function fetchPage(url, browserButton) {
 
 
 function fetchSection(url, browserButton) {
-  toggleContent(chapter_contents, true);
-  console.log(url);
+  var chapter_section = document.querySelector(".chapter-section");
+  toggleContent(chapter_section, true);
 
   var request = new XMLHttpRequest();
   request.open('GET', url, true);
@@ -138,6 +153,8 @@ function fetchSection(url, browserButton) {
 
 }
 
+
+
 // load html into our container
 function loadPage(contents, url, browserButton) {
   document.title = contents.querySelector("title").innerHTML;
@@ -153,19 +170,31 @@ function loadPage(contents, url, browserButton) {
   setTimeout(function() {
     scrollToTop(page_container);
     toggleContent(page_container, false);
-
-    chapter_links = document.querySelector(".chapter-links");
-    chapter_contents = document.querySelector(".chapter-contents");
-    console.log(chapter_links);
   }, 500)
 }
 
+
+
+
+
 function loadSection(section, url, browserButton) {
   document.title = section.querySelector("title").innerHTML;
-  console.log(section);
+  var chapter_section = document.querySelector(".chapter-section");
+
+  // get page navigation
+  var prevButton = document.querySelector(".prev a");
+  var nextButton = document.querySelector(".next a");
+
+  // update with section links
+  var prevHref = section.querySelector(".prev a").getAttribute("href");
+  var nextHref = section.querySelector(".next a").getAttribute("href");
+
+  prevButton.setAttribute("href", prevHref);
+  nextButton.setAttribute("href", nextHref);
+
   // add up css styles animation time for this duration
   setTimeout(function() {
-    chapter_contents.innerHTML = section.querySelector(".chapter-contents").innerHTML;
+    chapter_section.innerHTML = section.querySelector(".chapter-section").innerHTML;
   }, 200)
 
   pushState(browserButton, url);
@@ -173,7 +202,7 @@ function loadSection(section, url, browserButton) {
   // show new page
   setTimeout(function() {
     scrollToTop(page_container);
-    toggleContent(chapter_contents, false);
+    toggleContent(chapter_section, false);
   }, 500)
 
 }
@@ -202,9 +231,8 @@ function pushState(browserButton, url) {
 function setupPopState() {
   // popstate is triggered when forward or back button is clicked
   window.addEventListener("popstate", function(event) {
-    // get the directory/page path to jump to
+    // get the directory/page path to jump to .split('/').pop();
     var path = location.pathname.split('/').pop();
-    console.log(path);
     fetchPage(path, true);
   });
 }

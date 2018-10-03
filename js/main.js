23897faed04fa2af2ae916031211acfa6c4d7154
telagraphic <telagraphic@gmail.com>
2018-10-03 16:58:45 -0400

    updated html pages, refactored pjax very much, ready to combine

--- js/main.js
+++ js/main.js
@@ -1,108 +1,14 @@
-
-
-$('document').ready(function() {
-
-  // var transEffect = Barba.BaseTransition.extend({
-  //                 start: function(){
-  //                   this.newContainerLoading.then(val => this.fadeInNewcontent($(this.newContainer)));
-  //                 },
-  //                 fadeInNewcontent: function(nc) {
-  //                   nc.hide();
-  //                   var _this = this;
-  //                   $(this.oldContainer).fadeOut(1000).promise().done(() => {
-  //                     nc.css('visibility','visible');
-  //                     nc.fadeIn(1000, function(){
-  //                       _this.done();
-  //                     })
-  //                   });
-  //                 }
-  //             });
-  //             Barba.Pjax.getTransition = function() {
-  //               return transEffect;
-  //             }
-
-
-  var FadeTransition = Barba.BaseTransition.extend({
-    start: function() {
-      /**
-       * This function is automatically called as soon the Transition starts
-       * this.newContainerLoading is a Promise for the loading of the new container
-       * (Barba.js also comes with an handy Promise polyfill!)
-       */
-
-      // As soon the loading is finished and the old page is faded out, let's fade the new page
-      Promise
-        .all([this.newContainerLoading, this.fadeOut()])
-        .then(this.fadeIn.bind(this));
-    },
-
-    fadeOut: function() {
-      /**
-       * this.oldContainer is the HTMLElement of the old Container
-       */
-
-      return $(this.oldContainer).animate({ opacity: 0 }).promise();
-    },
-
-    fadeIn: function() {
-      /**
-       * this.newContainer is the HTMLElement of the new Container
-       * At this stage newContainer is on the DOM (inside our #barba-container and with visibility: hidden)
-       * Please note, newContainer is available just after newContainerLoading is resolved!
-       */
-
-      var _this = this;
-      var $el = $(this.newContainer);
-
-      $(this.oldContainer).hide();
-
-      $el.css({
-        visibility : 'visible',
-        opacity : 0
-      });
-
-      $el.animate({ opacity: 1 }, 200, function() {
-        /**
-         * Do not forget to call .done() as soon your transition is finished!
-         * .done() will automatically remove from the DOM the old Container
-         */
-
-        _this.done();
-      });
-    }
-  });
-
-  /**
-   * Next step, you have to tell Barba to use the new Transition
-   */
-
-  Barba.Pjax.getTransition = function() {
-    /**
-     * Here you can use your own logic!
-     * For example you can use different Transition based on the current page or link...
-     */
-
-    return FadeTransition;
-  };
-
-  Barba.Prefetch.init(); //use only with a short list of links....
-  Barba.Pjax.start();
-
-
-});
-
-
 var links = document.querySelectorAll('.main-nav a');
 
-links.forEach(function(link) {
-  link.addEventListener('click', function(event) {
+links.forEach(function (link) {
+  link.addEventListener('click', function (event) {
     removeActive(links);
     this.classList.add('active');
-  })
+  });
 });
 
 function removeActive(links) {
-  links.forEach(function(link) {
+  links.forEach(function (link) {
     link.classList.remove('active');
   });
 }
--- js/pjax.js
+++ js/pjax.js
@@ -1,60 +1,135 @@
-//
-// console.log("location.pathname:" + location.pathname);
-// console.log("location.hash:" + location.hash);
-// console.log("location.host:" + location.host);
-// console.log("location.href:" + location.href);
-// console.log("location.origin:" + location.origin);
-// console.log("location.port:" + location.port);
-// console.log("location.protocol:" + location.protocol);
-// console.log("location.search:" + location.search);
-
 //  get main-menu and intercept clicks
-var main_menu = document.querySelector(".main-menu");
-var page_container = document.querySelector(".page-container");
-var chapter_contents = document.querySelector(".chapter-contents");
+var mainMenu = document.querySelector('.main-menu');
+var pageContainer = document.querySelector('.page-container');
+var chapterContents = document.querySelector('.chapter-contents');
+var chapterSection = document.querySelector('.chapter-section');
 
+var request = {
+  button: false,
+  url: '',
+  type: '',
+};
 
-// container to intercept
-// link class to target
-// container to show/hide
+function linkInterceptor() {
+  window.addEventListener('click', function (event) {
 
-function setupChapterLinks() {
-  //main_menu
-  main_menu.addEventListener("click", function(event) {
-    if (event.target && event.target.matches("a.chapter-link")) {
+    // fetch new chapter
+    if (event.target && event.target.matches('a.chapter-link')) {
       event.preventDefault();
-      var linkURL = event.target.getAttribute("href");
-      if (preventDoubleClick(linkURL)) {
-        fetchPage(linkURL, false);
-      };
+      request.url = event.target.getAttribute('href');
+      request.type = 'page';
+      console.log(request);
+      if (preventDoubleClick(request.url)) {
+        fetchPage(request);
+      }
+    }
+
+    // fetch new chapter section
+    if (event.target && event.target.matches('a.section-link')) {
+      event.preventDefault();
+      request.url = event.target.getAttribute('href');
+      request.type = 'section';
+      if (preventDoubleClick(request.url)) {
+        fetchSection(request);
+      }
     }
   });
 }
 
-var chapter_links, chapter_contents;
+//  get external data from the link url
+function fetchPage(request) {
+  toggleContent(pageContainer, request.button);
 
-window.addEventListener("click", function(event) {
-  if (event.target && event.target.matches("a.sub-link")) {
-    event.preventDefault();
-    console.log("link clicked");
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', request.url, true);
 
-    var subURL = event.target.getAttribute("href");
-    if (preventDoubleClick(subURL)) {
-      fetchSection(subURL, false);
+  xhr.onload = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        var chapter = document.createElement('section');
+        chapter.innerHTML = xhr.responseText;
+        loadPage(chapter, request.url, request.button);
+      } else {
+        new Error('HTTP code is not 200');
+      }
     }
   };
-});
 
+  // https://github.com/nefe/You-Dont-Need-jQuery#query-selector
 
-window.addEventListener("click", function(event) {
-  if (event.target && event.target.matches("a.sub-page-link")) {
-    event.preventDefault();
-    var subURL = event.target.getAttribute("href");
-    if (preventDoubleClick(subURL)) {
-      fetchSection(subURL, false);
+  xhr.timeout = 5000;
+  xhr.ontimeout = function () { new Error('Request timed out'); };
+
+  xhr.onerror = function () { return 'Errored out!'; };
+
+  xhr.send();
+}
+
+function fetchSection(request) {
+  var chapterSection = document.querySelector('.chapter-section');
+  toggleContent(chapterSection, request.button);
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', request.url, true);
+
+  xhr.onload = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        var section = document.createElement('section');
+        section.innerHTML = xhr.responseText;
+        loadSection(section, request.url, request.button);
+      } else {
+        new Error('HTTP code is not 200');
+      }
     }
   };
-});
+
+  xhr.timeout = 5000;
+  xhr.ontimeout = function () { new Error('Request timed out'); };
+
+  xhr.onerror = function () { return 'Errored out!'; };
+
+  xhr.send();
+
+}
+
+// load html into our container
+function loadPage(contents, url, browserButton) {
+  document.title = contents.querySelector('title').innerHTML;
+
+  // add up css styles animation time for this duration
+  setTimeout(function () {
+    pageContainer.innerHTML = contents.querySelector('.page-container').innerHTML;
+  }, 200);
+
+  pushState(browserButton, url);
+
+  // show new page
+  setTimeout(function () {
+    scrollToTop(pageContainer);
+    toggleContent(pageContainer, true);
+  }, 500);
+}
+
+function loadSection(section, url, browserButton) {
+  document.title = section.querySelector('title').innerHTML;
+  var chapterSection = document.querySelector('.chapter-section');
+
+  updateNavigation(section);
+
+  // add up css styles animation time for this duration
+  setTimeout(function () {
+    chapterSection.innerHTML = section.querySelector('.chapter-section').innerHTML;
+  }, 200);
+
+  pushState(browserButton, url);
+
+  // show new page
+  setTimeout(function () {
+    scrollToTop(pageContainer);
+    toggleContent(chapterSection, true);
+  }, 500);
+}
 
 function preventDoubleClick(url) {
   var newURL = url.split('/').pop();
@@ -63,133 +138,38 @@ function preventDoubleClick(url) {
   if (newURL === path) {
     return false;
   }
+
   return true;
 }
 
-//  get external data from the link url
-function fetchPage(url, browserButton) {
-  toggleContent(page_container, true)
+function updateNavigation(section) {
 
-  // window.location = url;
-  var request = new XMLHttpRequest();
-  request.open('GET', url, true);
+  var prevHref = section.querySelector('.prev a').getAttribute('href');
+  var nextHref = section.querySelector('.next a').getAttribute('href');
 
-  request.onload = function() {
-    if (request.readyState === 4) {
-      if (request.status === 200) {
-        var chapter = document.createElement("section");
-        chapter.innerHTML = request.responseText;
-        loadPage(chapter, url, browserButton);
-      } else {
-        new Error("HTTP code is not 200");
-      }
-    }
-  };
+  var prevButtons = document.querySelectorAll('.prev a');
+  var nextButtons = document.querySelectorAll('.next a');
 
-  // https://github.com/nefe/You-Dont-Need-jQuery#query-selector
+  prevButtons.forEach(function (button) {
+    button.setAttribute('href', prevHref);
+  });
 
-  request.timeout = 5000;
-  request.ontimeout = function() { new Error("Request timed out"); }
-  request.onerror = function() { return "Errored out!" };
-  request.send();
-}
-
-
-function fetchSection(url, browserButton) {
-  var chapter_section = document.querySelector(".chapter-section");
-  toggleContent(chapter_section, true);
-
-  var request = new XMLHttpRequest();
-  request.open('GET', url, true);
-
-  request.onload = function() {
-    if (request.readyState === 4) {
-      if (request.status === 200) {
-        var section = document.createElement("section");
-        section.innerHTML = request.responseText;
-        loadSection(section, url, browserButton);
-      } else {
-        new Error("HTTP code is not 200");
-      }
-    }
-  };
-
-  request.timeout = 5000;
-  request.ontimeout = function() { new Error("Request timed out"); }
-  request.onerror = function() { return "Errored out!" };
-  request.send();
+  nextButtons.forEach(function (button) {
+    button.setAttribute('href', nextHref);
+  });
 
 }
-
-
-
-// load html into our container
-function loadPage(contents, url, browserButton) {
-  document.title = contents.querySelector("title").innerHTML;
-
-  // add up css styles animation time for this duration
-  setTimeout(function() {
-    page_container.innerHTML = contents.querySelector(".page-container").innerHTML;
-  }, 200)
-
-  pushState(browserButton, url);
-
-  // show new page
-  setTimeout(function() {
-    scrollToTop(page_container);
-    toggleContent(page_container, false);
-  }, 500)
-}
-
-
-
-
-
-function loadSection(section, url, browserButton) {
-  document.title = section.querySelector("title").innerHTML;
-  var chapter_section = document.querySelector(".chapter-section");
-
-  // get page navigation
-  var prevButton = document.querySelector(".prev a");
-  var nextButton = document.querySelector(".next a");
-
-  // update with section links
-  var prevHref = section.querySelector(".prev a").getAttribute("href");
-  var nextHref = section.querySelector(".next a").getAttribute("href");
-
-  prevButton.setAttribute("href", prevHref);
-  nextButton.setAttribute("href", nextHref);
-
-  // add up css styles animation time for this duration
-  setTimeout(function() {
-    chapter_section.innerHTML = section.querySelector(".chapter-section").innerHTML;
-  }, 200)
-
-  pushState(browserButton, url);
-
-  // show new page
-  setTimeout(function() {
-    scrollToTop(page_container);
-    toggleContent(chapter_section, false);
-  }, 500)
-
-}
-
 
 function toggleContent(container, animating) {
-  if (animating) {
+  if (!animating) {
     container.classList.add('animating');
   } else {
     container.classList.remove('animating');
   }
 }
 
-function hideContent(container) {
-  container.classList.add('animating');
-}
-
 function scrollToTop(container) {
-  container.scroll({ top: 0, behavior: "instant"})
+  container.scroll({ top: 0, behavior: 'instant' });
 }
 
 function pushState(browserButton, url) {
@@ -197,14 +177,12 @@ function pushState(browserButton, url) {
 }
 
 function setupPopState() {
-  // popstate is triggered when forward or back button is clicked
-  window.addEventListener("popstate", function(event) {
-    // get the directory/page path to jump to .split('/').pop();
-    var path = location.pathname.split('/').pop();
-    fetchPage(path, true);
+  window.addEventListener('popstate', function (event) {
+    request.url = location.pathname.split('/').pop();
+    request.button = true;
+    fetchPage(request);
   });
 }
 
-
-setupChapterLinks();
+linkInterceptor();
 setupPopState();

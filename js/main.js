6414b39ccbd67261ca48b7251d4d277eab164b6b
telagraphic <telagraphic@gmail.com>
2018-11-03 14:51:46 -0400

    updated

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+var vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', function (event) {
+  // We execute the same script as before
+  var vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 var links = document.querySelectorAll('.main-nav a');
 
 links.forEach(function (link) {
